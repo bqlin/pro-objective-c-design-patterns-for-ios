4ce61cb003812e88ad8ece0b1fb86a0cdd162c6f
LinBq <bq_lin@hotmail.com>
2017-06-07 17:08:19 +0800

    # 新增 - CanvasViewGenerator

---
+++ TouchPainter_Refactor/TouchPainter/Classes/FactoryMethod/CanvasViewGenerator.h
@@ -0,0 +1,16 @@
+//
+//  CanvasViewGenerator.h
+//  TouchPainter
+//
+//  Created by LinBq on 2017/6/7.
+//  Copyright © 2017年 LinBq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CanvasView.h"
+
+@interface CanvasViewGenerator : NSObject
+
+- (CanvasView *)canvasViewWithFrame:(CGRect)frame;
+
+@end
---
+++ TouchPainter_Refactor/TouchPainter/Classes/FactoryMethod/CanvasViewGenerator.m
@@ -0,0 +1,17 @@
+//
+//  CanvasViewGenerator.m
+//  TouchPainter
+//
+//  Created by LinBq on 2017/6/7.
+//  Copyright © 2017年 LinBq. All rights reserved.
+//
+
+#import "CanvasViewGenerator.h"
+
+@implementation CanvasViewGenerator
+
+- (CanvasView *)canvasViewWithFrame:(CGRect)frame {
+    return [[CanvasView alloc] initWithFrame:frame];
+}
+
+@end
