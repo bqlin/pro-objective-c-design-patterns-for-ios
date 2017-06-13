f6f33fa1d4cd43a9ecb229e7bd2d4dc2ac286fdb
LinBq <bq_lin@hotmail.com>
2017-06-13 17:54:21 +0800

    添加保存逻辑

--- TouchPainter_Refactor/TouchPainter/Classes/Command/SaveScribbleCommand.m
+++ TouchPainter_Refactor/TouchPainter/Classes/Command/SaveScribbleCommand.m
@@ -8,11 +8,33 @@
 
 #import "SaveScribbleCommand.h"
 #import "ScribbleManager.h"
+#import "CoordinatingController.h"
+#import "UIView+UIImage.h"
 
 @implementation SaveScribbleCommand
 
 - (void)execute {
+    // get a hold of all necessary information
+    // from an instance of CanvasViewController
+    // for saving its Scribble
+    CoordinatingController *coordinatingController = [CoordinatingController sharedInstance];
+    CanvasViewController *canvasViewController = coordinatingController.canvasViewController;
+    Scribble *scribble = canvasViewController.scribble;
+    UIImage *canvasViewImage = canvasViewController.canvasView.image;
     
+    // use an instance of ScribbleManager
+    // to save the scribble and its thumbnail
+    ScribbleManager *scribbleManager = [[ScribbleManager alloc] init];
+    [scribbleManager saveScribble:scribble thumbnail:canvasViewImage];
+    
+    // finally show an alertbox that says
+    // after the scribble is saved
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Your scribble is saved"
+                                                        message:nil
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+    [alertView show];
 }
 
 @end
